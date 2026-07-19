fa4d1077eaf3a563c9f0fdff53b302ffa93fc1b1
Amirparsa_g <Amirparsa_g@yahoo.com>
2026-07-19 10:28:03 +0330

    feat: Tamrin 5 , 6:Adding a new user/putting Users in state

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,36 @@
 import { useState } from "react";
-// import UserList from "./components/UserList";
-import { users } from "./data/users";
-import type { User } from "./types/user";
 import "./App.css";
 import UserList from "./components/UserList";
-
+import type { User } from "./types/user";
+import users from "./data/users";
 function App() {
-  const [userList, setUserList] = useState<User[]>(users);
-  function userAddHandler() {
-    const newUser: User = {
-      ID: 7,
-      fullName: "کاربر آزمایشی",
-      age: 27,
-      role: "operator",
-      isActive: true,
-      email: "test.user@example.com",
-    };
-    if (userList.some((user) => user.ID === newUser.ID))
-      alert("this user alredy exists");
-    setUserList([...userList, newUser]);
-    alert("user added successfully");
-  }
+  const [UsersList, setUserList] = useState<User[]>(users);
+  const newUser: User = {
+    ID: 7,
+    fullName: "کاربر آزمایشی",
+    age: 27,
+    role: "operator",
+    isActive: true,
+    email: "test.user@example.com",
+  };
+  const addUserHandler = (newUser: User) => {
+    const isAvailable = UsersList.find((user) => user.ID === newUser.ID);
+    if (isAvailable) alert("you cant add the same user twice");
+    else {
+      setUserList([...UsersList, newUser]);
+      alert("user Added Successfully");
+    }
+  };
   return (
-    <div className="flex bg-amber-200">
-      <UserList users={users} />
-      <button onClick={userAddHandler}>click</button>
-    </div>
+    <>
+      <UserList users={UsersList} />
+      <button
+        onClick={() => addUserHandler(newUser)}
+        className="bg-slate-50 block m-auto mt-5 border border-purple-500 p-3 rounded-2xl"
+      >
+        Add New User
+      </button>
+    </>
   );
 }
 
