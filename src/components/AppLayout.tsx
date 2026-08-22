2c09e255e8779e425651fa13b563d3ed6cbd3738
Amirparsa_g <Amirparsa_g@yahoo.com>
2026-08-22 13:16:02 +0330

    feat: added project's colors and fonts into index.css, added sidebar and bottom navigation

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,76 +1,37 @@
 import { useState } from "react";
-import { Outlet, NavLink } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import hamburgerIcon from "../assets/hamburger-icon.svg";
+import DesktopSidebar from "./DesktopSidebar";
+import ButtomNav from "./ButtomNav";
+
 const AppLayout = () => {
   const [isHamburger, setIsHamburger] = useState<boolean>(false);
   return (
-    <div className="min-h-1">
-      <div className="grid grid-cols-1 md:grid-cols-2">
-        <header className="w-full h-10 flex justify-center items-center my-4">
+    <div className="flex w-full h-screen overflow-hidden">
+      {!isHamburger && (
+        <div className="flex justify-start items-start mt-6.5 mx-2 md:hidden">
+          <button onClick={() => setIsHamburger(true)}>
+            <img src={hamburgerIcon} alt="hambutger" />
+          </button>
+        </div>
+      )}
+
+      <DesktopSidebar
+        isHamburger={isHamburger}
+        setIsHamburger={setIsHamburger}
+      />
+      <div className="flex flex-1 flex-col h-full overflow-hidden">
+        <header className="w-full h-10 flex justify-center items-center my-4 shrink-0">
           <h1 className="text-center text-2xl font-semibold">
             User Managment Pannel Project
           </h1>
         </header>
-        <span className="sm:hidden">
-          <button
-            className="block  sm:hidden ml-3"
-            onClick={() => setIsHamburger((prev) => !prev)}
-          >
-            <img src={hamburgerIcon} alt="hamburger icon" />
-          </button>
-        </span>
-        <nav
-          className={`${isHamburger ? "flex" : "hidden"} flex-col max-w-fit ml-3 sm:flex sm:flex-row sm:justify-center sm:items-center sm:gap-3 sm:my-4 sm:mx-auto`}
-        >
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive
-                ? "bg-blue-400 border border-blue-700 p-2 hover:scale-105 text-slate-100 transition-all ease-in-out duration-300 rounded-md"
-                : "bg-slate-100 border border-blue-400 p-2 hover:scale-105 transition-all ease-in-out duration-300 rounded-md"
-            }
-          >
-            Home Page
-          </NavLink>
-          <NavLink
-            to="/users"
-            end
-            className={({ isActive }) =>
-              isActive
-                ? "bg-blue-400 border border-blue-700 p-2 hover:scale-105 text-slate-100 transition-all ease-in-out duration-300 rounded-md"
-                : "bg-slate-100 border border-blue-400 p-2 hover:scale-105 transition-all ease-in-out duration-300 rounded-md"
-            }
-          >
-            Users
-          </NavLink>
-          <NavLink
-            to="/users/new"
-            className={({ isActive }) =>
-              isActive
-                ? "bg-blue-400 border border-blue-700 p-2 hover:scale-105 text-slate-100 transition-all ease-in-out duration-300 rounded-md"
-                : "bg-slate-100 border border-blue-400 p-2 hover:scale-105 transition-all ease-in-out duration-300 rounded-md"
-            }
-          >
-            Add User
-          </NavLink>
-          <NavLink
-            to="/about"
-            className={({ isActive }) =>
-              isActive
-                ? "bg-blue-400 border border-blue-700 p-2 hover:scale-105 text-slate-100 transition-all ease-in-out duration-300 rounded-md"
-                : "bg-slate-100 border border-blue-400 p-2 hover:scale-105 transition-all ease-in-out duration-300 rounded-md"
-            }
-          >
-            About Project
-          </NavLink>
-        </nav>
+
+        <main className=" flex flex-1 flex-col justify-start items-center p-3 w-full overflow-y-auto mb-10">
+          <Outlet />
+        </main>
       </div>
-      <main className=" flex flex-col justify-center items-center p-3 min-h-screen">
-        <Outlet />
-      </main>
-      <footer className="border-t-2 border-purple-400 my-5 bg-slate-50 h-50">
-        <h2 className="text-center text-3xl text-cyan-400 ">Footer</h2>
-      </footer>
+      <ButtomNav />
     </div>
   );
 };
---
+++ src/components/DesktopSidebar.tsx
@@ -0,0 +1,78 @@
+import { NavLink } from "react-router-dom";
+import homeIcon from "../assets/home.svg";
+import usersIcon from "../assets/users.svg";
+import addUserIcon from "../assets/add.svg";
+import aboutIcon from "../assets/about.svg";
+import arrowBackIcon from "../assets/arrow-back.svg";
+const DesktopSidebar = ({
+  isHamburger,
+  setIsHamburger,
+}: {
+  isHamburger: boolean;
+  setIsHamburger: (value: boolean) => void;
+}) => {
+  return (
+    <aside
+      className={`${isHamburger ? "aside-hamburger" : "aside-notHamburger"}`}
+    >
+      <h2 className="text-body font-header2 text-white">User Mangement</h2>
+      <nav className="nav-desktop">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? "nav-btn-selected" : "nav-btn-notSelected"
+          }
+        >
+          <div className="flex gap-1">
+            <img src={homeIcon} alt="Home" className="nav-img" />
+            <p>Home</p>
+          </div>
+        </NavLink>
+        <NavLink
+          to="/users"
+          end
+          className={({ isActive }) =>
+            isActive ? "nav-btn-selected" : "nav-btn-notSelected"
+          }
+        >
+          <div className="flex gap-1">
+            <img src={usersIcon} alt="Home" className="nav-img" />
+            <p>Users</p>
+          </div>
+        </NavLink>
+        <NavLink
+          to="/users/new"
+          className={({ isActive }) =>
+            isActive ? "nav-btn-selected" : "nav-btn-notSelected"
+          }
+        >
+          <div className="flex gap-1">
+            <img src={addUserIcon} alt="Home" className="nav-img" />
+            <p>Add User</p>
+          </div>
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "nav-btn-selected" : "nav-btn-notSelected"
+          }
+        >
+          <div className="flex gap-1">
+            <img src={aboutIcon} alt="Home" className="nav-img" />
+            <p>About</p>
+          </div>
+        </NavLink>
+      </nav>
+      {isHamburger && (
+        <button
+          onClick={() => setIsHamburger(false)}
+          className="fixed bottom-15"
+        >
+          <img src={arrowBackIcon} alt="back" className="nav-img" />
+        </button>
+      )}
+    </aside>
+  );
+};
+
+export default DesktopSidebar;
