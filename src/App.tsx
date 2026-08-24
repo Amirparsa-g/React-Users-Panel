5e56ed2eb96e34ffd109621f229b46760f4b9598
Amirparsa_g <Amirparsa_g@yahoo.com>
2026-08-24 15:23:59 +0330

    fix : fixed remove user confirm , feat: added slide-out animation

--- src/App.tsx
+++ src/App.tsx
@@ -62,14 +62,15 @@ function App() {
   const removeUserHandler = (id: number) => {
     const remainingUsers = UsersList.filter((user) => user.ID !== id);
     setUserList(remainingUsers);
-    confirm("are you sure you want to remove this use?");
   };
   const removeApiUser = async (id: number) => {
     try {
       setError(null);
-      const deleteResponse = await deleteApiUser(id);
-      if (deleteResponse.isDeleted) removeUserHandler(id);
-      return true;
+      if (confirm("are you sure you want to remove this use?")) {
+        const deleteResponse = await deleteApiUser(id);
+        if (deleteResponse.isDeleted) removeUserHandler(id);
+        return true;
+      }
     } catch (error) {
       if (error instanceof Error) setError(error.message);
       else setError("Unexpected Error");
