74e4f0c16e0d30a358a41685b42af549aa2972bd
Amirparsa_g <Amirparsa_g@yahoo.com>
2026-09-05 14:34:49 +0330

    fix : fixed the actions of the users in the desktop size

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -82,8 +82,11 @@ const UserCard = ({
             </span>
           </td>
           <td>
-            <div className="hidden md:flex gap-1 ">
-              <Link to={`/users/${user.ID}`} className="cursor-pointer">
+            <div className="hidden md:flex gap-1 justify-between">
+              <Link
+                to={`/users/${user.ID}`}
+                className="cursor-pointer relative group flex items-center justify-center"
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   height="24px"
@@ -93,6 +96,10 @@ const UserCard = ({
                 >
                   <path d="M607.5-372.5Q660-425 660-500t-52.5-127.5Q555-680 480-680t-127.5 52.5Q300-575 300-500t52.5 127.5Q405-320 480-320t127.5-52.5Zm-204-51Q372-455 372-500t31.5-76.5Q435-608 480-608t76.5 31.5Q588-545 588-500t-31.5 76.5Q525-392 480-392t-76.5-31.5ZM214-281.5Q94-363 40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200q-146 0-266-81.5ZM480-500Zm207.5 160.5Q782-399 832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280q113 0 207.5-59.5Z" />
                 </svg>
+                {/* پیام پاپ‌آپ */}
+                <div className="absolute top-full mt-2 left-1/2 -translate-x-1/2 hidden group-hover:block bg-gray-900 text-white text-xs px-2.5 py-1.5 rounded shadow-lg whitespace-nowrap z-50">
+                  View user
+                </div>
               </Link>
               <button
                 disabled={updatingUserId === user.ID}
@@ -101,17 +108,21 @@ const UserCard = ({
                   await onRemove(user.ID);
                   setUpdatingUserId(null);
                 }}
-                className="cursor-pointer"
+                className="cursor-pointer relative group flex items-center justify-center"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   height="24px"
                   viewBox="0 -960 960 960"
                   width="24px"
-                  fill="#1f1f1f"
+                  fill="#ef4444"
                 >
                   <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
                 </svg>
+                {/* پیام پاپ‌آپ */}
+                <div className="absolute top-full mt-2 left-1/2 -translate-x-1/2 hidden group-hover:block bg-gray-900 text-white text-xs px-2.5 py-1.5 rounded shadow-lg whitespace-nowrap z-50">
+                  Remove user
+                </div>
               </button>
               <button
                 disabled={updatingUserId === user.ID}
@@ -120,7 +131,7 @@ const UserCard = ({
                   await changeStatus(user);
                   setUpdatingUserId(null);
                 }}
-                className="cursor-pointer"
+                className="cursor-pointer relative group flex items-center justify-center"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -131,6 +142,10 @@ const UserCard = ({
                 >
                   <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" />
                 </svg>
+                {/* پیام پاپ‌آپ */}
+                <div className="absolute top-full mt-2 left-1/2 -translate-x-1/2 hidden group-hover:block bg-gray-900 text-white text-xs px-2.5 py-1.5 rounded shadow-lg whitespace-nowrap z-50">
+                  Change status
+                </div>
               </button>
             </div>
           </td>
