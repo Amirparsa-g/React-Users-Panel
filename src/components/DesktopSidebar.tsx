eb4b4ffd45222d5733cc59bd60f3e45707d9026f
Amirparsa_g <Amirparsa_g@yahoo.com>
2026-09-16 16:05:03 +0330

    fix : fixed the sidebar  stiles in farsi mode and mobile size

--- src/components/DesktopSidebar.tsx
+++ src/components/DesktopSidebar.tsx
@@ -18,12 +18,14 @@ const DesktopSidebar = ({
 }) => {
   const [isHover, setIsHover] = useState<boolean>(false);
   const { t } = useTranslation();
+  const { i18n } = useTranslation();
+  const isFa = i18n.language === "fa";
 
   return (
     <aside
       onMouseEnter={() => setIsHover(true)}
       onMouseLeave={() => setIsHover(false)}
-      className={` transition-all ${!isMobile ? ` ${isHover || isClicked ? " hidden md:aside-notHamburger" : " hidden md:aside-notHamburger md:w-20  "}` : ` md:hidden ${isClicked ? "aside-notHamburger md:aside-notHamburger animate-slide-in" : " aside-notHamburger -translate-x-60 "}`}`}
+      className={` transition-all ${!isMobile ? ` ${isHover || isClicked ? " hidden md:aside-notHamburger" : " hidden md:aside-notHamburger md:w-20  "}` : ` md:hidden ${isClicked ? `aside-notHamburger md:aside-notHamburger ${!isFa ? "animate-slide-in" : "animate-slide-in-fa"}` : " hidden md:aside-notHamburger -translate-x-60 "}`}`}
     >
       <div className="flex gap-2  items-center p-2 ">
         <div className="bg-sidebar-2 p-2 rounded-xl w-10 h-10 flex justify-center items-center border border-white">
