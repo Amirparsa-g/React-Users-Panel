e7b30a6257f6842013404ae2f573d3757766b00d
Amirparsa_g <Amirparsa_g@yahoo.com>
2026-09-16 14:55:33 +0330

    feat : added fa language to loading and user error components

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,12 @@
+import { useTranslation } from "react-i18next";
+
 const Loading = () => {
+  const { t } = useTranslation();
   return (
     <div className="flex flex-col userStats-card border border-dotted w-full">
-      <p className="text-small font-bold mb-5 text-text-primary">Loading...</p>
+      <p className="text-small font-bold mb-5 text-text-primary">
+        {t("common.loading")}
+      </p>
       <div className="skeleton"></div>
       <div className="skeleton"></div>
     </div>
--- src/components/UserError.tsx
+++ src/components/UserError.tsx
@@ -1,17 +1,19 @@
+import { useTranslation } from "react-i18next";
 import Buttons from "./Buttons";
 
 const UserError = ({ LoadUser }: { LoadUser: () => void }) => {
+  const { t } = useTranslation();
   return (
     <div className="flex flex-col userStats-card border border-dotted w-full">
-      <p className="text-small font-bold">Error</p>
-      <p className="caption mb-5">Could not load users. </p>
+      <p className="text-small font-bold">{t("common.error")}</p>
+      <p className="caption mb-5">{t("pages.errors.userError")} </p>
       <Buttons
         comp="button"
         onClick={LoadUser}
         buttonType="neutral"
         more="dark:text-white"
       >
-        Retry
+        {t("common.retry")}
       </Buttons>
     </div>
   );
