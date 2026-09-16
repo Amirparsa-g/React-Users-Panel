b695f534351d361cdb11f51358a4ab299c259321
Amirparsa_g <Amirparsa_g@yahoo.com>
2026-09-16 14:48:41 +0330

    feat : added fa language to Empty state component

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -1,19 +1,21 @@
+import { useTranslation } from "react-i18next";
 import Buttons from "./Buttons";
 
 const EmptyState = () => {
+  const { t } = useTranslation();
   return (
     <div className="flex flex-col userStats-card border border-dotted w-full">
-      <p className="text-small font-bold text-text-primary">Empty</p>
-      <p className="caption mb-5">
-        No users match the current search or filters.
+      <p className="text-small font-bold text-text-primary">
+        {t("pages.empty.title")}
       </p>
+      <p className="caption mb-5">{t("pages.empty.message")}</p>
       <Buttons
         buttonType="primary"
         comp="link"
         navigation={"/users/new"}
         className="primary-button p-2 w-fit"
       >
-        Add User
+        {t("pages.users.usersPageAddUserButton")}
       </Buttons>
     </div>
   );
