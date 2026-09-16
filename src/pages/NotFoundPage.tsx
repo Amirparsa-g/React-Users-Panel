d2bcd1d0915eee9e93df16ab1a0c91ca56264b54
Amirparsa_g <Amirparsa_g@yahoo.com>
2026-09-16 14:25:01 +0330

    feat : added fa language to notFoundPage

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,8 +1,10 @@
 import { useEffect } from "react";
 
 import Buttons from "../components/Buttons";
+import { useTranslation } from "react-i18next";
 
 const NotFoundPage = () => {
+  const { t } = useTranslation();
   useEffect(() => {
     document.title = "404 | User Management";
   }, []);
@@ -10,11 +12,11 @@ const NotFoundPage = () => {
     <div className="userStats-card w-full">
       <h2 className="text-[160px] text-center text-gray-400">404</h2>
       <p className="text-xl font-bold text-center mt-5 text-text-primary">
-        Page Not Found
+        {t("pages.notFound.message")}
       </p>
       <div className="flex justify-evenly items-center mt-30 ">
         <Buttons comp="link" buttonType="backTo" navigation="/users">
-          <span>&larr;</span> Back to Users
+          <span>&larr;</span> {t("common.backToUsers")}
         </Buttons>
       </div>
     </div>
